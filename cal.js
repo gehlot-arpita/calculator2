a085e39756027bc7671fa12ea568d5d29483581e
Arpita Gehlot <128213441+gehlot-arpita@users.noreply.github.com>
2023-04-19 11:05:03 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,93 @@
+function f1()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"7";
+        }
+	function f2()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"8";
+        }
+	function f3()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"9";
+        }
+	function f4()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"-";
+        }
+        function f5()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"4";
+        }
+	function f6()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"5";
+        }
+	function f7()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"6";
+        }
+	function f8()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"+";
+        }
+	function f9()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"1";
+        }
+	function f10()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"2";
+        }
+	function f11()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"3";
+        }
+	function f12()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"%";
+        }
+	function f13()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"0";
+        }
+	function f14()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+".";
+        }
+	function f15()
+	{
+	document.getElementById("num").value="";
+	}
+	function f16()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"*";
+        }
+	function f17()
+	{
+	pre=document.getElementById("num").value;
+	document.getElementById("num").value=pre+"/";
+        }
+	function fc()
+	{
+	document.getElementById("num").value;	
+	}
+	function fe()
+	{
+	aa=document.getElementById("num").value;
+	document.getElementById("num").value=eval(aa);
+	}
